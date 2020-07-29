e97561c908e56e6c8f742bb438393358f96367a9
Kristin Barr <kristinbarr@gmail.com>
2020-07-28 22:47:21 -0400

    fixed DB+_ENV variable

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -2,6 +2,6 @@ const knex = require('knex');
 
 const knexfile = require('../knexfile');
 
-const dbEnv = process.env.DATABASE_URL || 'development'
+const dbEnv = process.env.DB_ENV || 'development'
 
 module.exports = knex(knexfile[dbEnv]);
